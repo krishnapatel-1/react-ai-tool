9b38443c34102efaf19060cb1d40584d3d43a5f5
krishnapatel-1 <krishna9329ptl@gmail.com>
2025-08-05 01:36:24 +0530

    adding delet button

--- src/App.jsx
+++ src/App.jsx
@@ -53,16 +53,25 @@ else {
   };
 
   console.log(result);
+   
+ const clearhistory=()=>{
+  localStorage.clear();
+  setRecentHistory([])
+ }
 
   return (
     <div className="grid grid-cols-5 h-screen text-center ">
       <div className="col-span-1 bg-zinc-800  ">
-         
-<ul>
+         <h1 className="p-3 text-white text-xl flex text-center justify-center  ">
+          <span>Recent Search</span>
+<button  onClick={clearhistory} className="cursor-pointer"  ><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg></button>
+
+         </h1>
+<ul className="text-left overflow-auto">
 
   {
   recentHistory && recentHistory.map((item)=>(
-    <li>{ item } 
+    <li className="p-1 pl-4 truncate  text-zinc-400 cursor-pointer hover:bg-zinc-700 hover:text-white  ">{ item } 
     </li>
     ))
   }
